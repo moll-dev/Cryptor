bb5b5399114930e449c7df04f6f97bada867fab8
ThomasMoll <thomas.moll@webfilings.com>
2014-04-16 15:54:36 -0500

    Added key generation (dropbox acct, and gpg)
    Need to work on making passwords secure when in memory and otherwise

--- src/main.py
+++ src/main.py
@@ -16,9 +16,14 @@ keyfile = "keychains/keychain.data"
 try:
 	keychain = Keychain(keyfile)
 except EnvironmentError:
-	generate_keys(APP_KEY, APP_SECRET)
-	print "That keychain file does not exist"
+	print "That keychain file does not exist!"
+	ans = raw_input("Would you like to make one? (Y/n):")
+	if ans.upper() == "Y":
+		generate_keys(APP_KEY, APP_SECRET)
+	else:
+		print "Boo you didn't want to make any keys"
 
+print "You have a good key and you're ready to GO!"
 
 
 """Create client object"""
--- src/util.py
+++ src/util.py
@@ -1,4 +1,4 @@
-import dropbox, webbrowser, gnupg
+import dropbox, webbrowser, gnupg, sys, os, json
 
 class Debugger():
     def __init__(self, DEBUG_ON=False):
@@ -13,14 +13,15 @@ class Debugger():
 
 def generate_keys(key, secret, gpg_size=2048, key_space="/keychains", filename="keychain.data"):
 
-    #dropbox_key = generate_client_key(key, secret)
-    gpg_key = generate_gpg_key(key_space)
+    dropbox_key = generate_client_key(key, secret)
+    gpg_key = generate_gpg_key(os.getcwd()+key_space)
 
-    token_file = open(key_space+filename,'w')
+    token_file = open(os.getcwd()+key_space+'/'+filename,'w')
     
-    #keys = {'client_key': access_token.key, 'client_secret': access_token.secret}
-    #token_file.write(json.dumps(keys))
-    #token_file.close()
+    keys = {'client_key': dropbox_key.key, 'client_secret': dropbox_key.secret,
+           'gpg_private': gpg_key['private'],'gpg_public': gpg_key['public']}
+    token_file.write(json.dumps(keys))
+    token_file.close()
     print("Token Written to file!\n\n")
 
 def generate_client_key(key, secret):
@@ -44,14 +45,19 @@ def generate_gpg_key(gpg_home):
 
     #    input_data = gpg.gen_key_input(key_type="RSA", key_length=gpg_size, )
     """Put asking for key values here!"""
-    params = {'key_length':"",'name_real':"",'passphrase':""}
-
+    params = {'key_length':"2048",'name_real':"Cryptor",'passphrase':""}
+    #TODO MAKE GETTING PASSWORD MORE SECURE
+    params['passphrase'] = raw_input('Enter passphrase: ')
     input_data = gpg.gen_key_input(**params)
     key = gpg.gen_key(input_data)
 
-    print key
+    return {'private':gpg.export_keys(key.fingerprint, True),'public':gpg.export_keys(key.fingerprint)}
 
+def load_gpg_keys(): #TODO
+    pass
 
+def verify_keyfile(): #TODO: Verify that the keyfile given is valid.
+    pass
 
 def debug(msg):
     if DEBUG_ON:
